8d304a2821914cf9b6961d6aac8ed2c0f0d1af77
HoussemCherif <46661326+HoussemCherif@users.noreply.github.com>
2019-01-14 19:58:46 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import logo from './logo.jpeg';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>
+        Helloe the world</p>
+        
+        
+        <img src = {logo}/>
+       
+        <br>
+        
+        </br>
+        
+        <hr>
+        </hr>
+        
+        
+        
+        
+        
+        <p class = "paragraphe">
+        Salut Tous Le Monde</p>
+        
+        
+      </div>
+    );
+  }
+}
+export default App;
